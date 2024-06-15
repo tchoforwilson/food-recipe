3bcb16edc4aa0196f9fa09f8ee2917c383635934
tchoforwilson <tchoforwilson@gmail.com>
2024-06-15 17:16:24 +0100

    Application stack authentication navigation

---
+++ src/navigations/AuthNavigator.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { WelcomeScreen } from "../screens";
+
+const Stack = createStackNavigator();
+
+const AuthNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthNavigator;
