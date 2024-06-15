786486304c2213340f62b740328b091353bbf043
tchoforwilson <tchoforwilson@gmail.com>
2024-06-15 17:15:32 +0100

    Application main text field

--- src/components/texts/AppText.js
+++ src/components/texts/AppText.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import defaultStyles from "../../configurations/styles";
 
 const AppText = ({ children, style, ...otherProps }) => {
-  return <Text style={[style]}>{children}</Text>;
+  return (
+    <Text style={[styles.text, style]} {...otherProps}>
+      {children}
+    </Text>
+  );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    color: defaultStyles.COLORS.white,
+    fontSize: defaultStyles.THEMES.SIZES.base,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+});
 
 export default AppText;
