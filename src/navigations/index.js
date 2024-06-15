dbc53c659f9fd1f6f6803679996b6025de237588
tchoforwilson <tchoforwilson@gmail.com>
2024-06-15 17:17:26 +0100

    Index export

---
+++ src/navigations/index.js
@@ -0,0 +1,3 @@
+export { default as NavigationTheme } from "./NavigationTheme";
+export { default as AppNavigator } from "./AppNavigator";
+export { default as AuthNavigator } from "./AuthNavigator";
