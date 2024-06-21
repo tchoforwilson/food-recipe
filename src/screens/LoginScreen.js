f19191b8ae1656ee0c0e58f83bf4b4446282d915
tchoforwilson <tchoforwilson@gmail.com>
2024-06-21 14:44:48 +0100

    Login screen setup

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const LoginScreen = (props) => {
+  return <View style={styles.container}>Login screen</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default LoginScreen;
