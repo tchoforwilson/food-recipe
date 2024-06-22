1f28b7970bd29281a63efc899f4cb033e7b01423
tchoforwilson <tchoforwilson@gmail.com>
2024-06-22 11:20:26 +0100

    Default export screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,4 @@
-export { default as HomeScreen } from "./HomeScreen";
-export { default as LoginScreen } from "./LoginScreen";
 export { default as WelcomeScreen } from "./WelcomeScreen";
+export { default as LoginScreen } from "./LoginScreen";
+export { default as HomeScreen } from "./HomeScreen";
+export { default as RecipeScreen } from "./RecipeScreen";
